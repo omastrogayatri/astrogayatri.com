58755eaca9ba7941b85323a8f824513f3db86f80
Abhishek Kumar <omastrogayatri@gmail.com>
2025-05-06 13:05:02 +0800

    Seventh Commit

--- astrohomepage.py
+++ astrohomepage.py
@@ -1,14 +1,45 @@
-from flask import Flask, request, render_template
-import requests
+from flask import Flask, render_template, request, jsonify
+import csv
+from collections import defaultdict
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+# Load CSV data into nested dict structure
+locations = defaultdict(lambda: defaultdict(set))
 
-@app.route('/generate', methods=['POST'])
-def generate_kundali():
+with open('locations.csv', newline='', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        country = row['country']
+        state = row['state']
+        city = row['city']
+        locations[country][state].add(city)
+
+# Convert sets to lists for JSON serialization
+for country in locations:
+    for state in locations[country]:
+        locations[country][state] = list(locations[country][state])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_states')
+def get_states():
+    country = request.args.get('country')
+    states = list(locations.get(country, {}).keys())
+    return jsonify(states)
+
+@app.route('/get_cities')
+def get_cities():
+    state = request.args.get('state')
+    for country_data in locations.values():
+        if state in country_data:
+            return jsonify(country_data[state])
+    return jsonify([])
+
+@app.route('/submit', methods=['POST'])
+def submit():
     name = request.form['name']
     dob = request.form['dob']
     tob = request.form['tob']
