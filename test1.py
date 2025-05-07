78e21eb92a2e19b883d017ffbc2771662b0646fa
Abhishek Kumar <omastrogayatri@gmail.com>
2025-05-07 18:18:39 +0800

    20th commit

--- test1.py
+++ test1.py
@@ -1,7 +1,4 @@
 from skyfield.api import load
 
-planets = load('de440s.bsp')  # or your full path
-
-print("Available targets in the kernel:")
-for target in planets.names():
-    print(target)
+planets = load('de440s.bsp')
+print("Loaded planets:", planets.names())
