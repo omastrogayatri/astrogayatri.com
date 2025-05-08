d79b001e6bc0412470052eb8ea801dad82bee761
Abhishek Kumar <omastrogayatri@gmail.com>
2025-05-08 22:52:25 +0800

    multiple bsp file added to py3

--- astrohomepage.py
+++ astrohomepage.py
@@ -94,8 +94,8 @@ def submit():
     BSP_URL1 = 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de421.bsp'
     BSP_FILE2 = 'sat454.bsp'
     BSP_URL2 = 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/satellites/sat454.bsp'
-    BSP_FILE3 = 'jup344.bsp'
-    BSP_URL3 = 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/satellites/jup344.bsp'
+    BSP_FILE3 = 'jup346.bsp'
+    BSP_URL3 = 'https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/satellites/jup346.bsp'
 
 # Download if file is missing
     if not os.path.exists(BSP_FILE1):
@@ -109,7 +109,7 @@ def submit():
      print("Download complete.")
 
     if not os.path.exists(BSP_FILE3):
-     print("Downloading jup344.bsp")
+     print("Downloading jup346.bsp")
      urllib.request.urlretrieve(BSP_URL3, BSP_FILE3)
      print("Download complete.")
      
