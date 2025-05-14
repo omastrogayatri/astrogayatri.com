e87d385d34a5aa54dd70bac38fa36006a212ad29
Abhishek Kumar <omastrogayatri@gmail.com>
2025-05-14 21:35:36 +0800

    14thmay2503

--- astrohomepage.py
+++ astrohomepage.py
@@ -210,7 +210,7 @@ def submit():
 
     #Together AI call
     TOGETHER_API_KEY = "tgp_v1_xekeicYc2vvaUxUnpuWODX3q1LLMf-x4MCO5HYY6mmc"
-    TOGETHER_MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"  # You can change model
+    TOGETHER_MODEL = "deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"  # You can change model
  
     
 
