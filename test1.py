06de34191b1d7291af8fbd0a6e2fb8a143ed2474
Abhishek Kumar <omastrogayatri@gmail.com>
2025-05-17 21:16:34 +0800

    17thmay

--- test1.py
+++ test1.py
@@ -8,7 +8,7 @@ import urllib.request
 TOGETHER_API_KEY = "tgp_v1_xekeicYc2vvaUxUnpuWODX3q1LLMf-x4MCO5HYY6mmc"
 TOGETHER_MODEL = "mistralai/Mistral-7B-Instruct-v0.2"  # You can change model
 prompt = f"""
-    Donot include any reasoning steps or thoughts, Generate a short 5 lines essay on dogs , do not include chain of thought or think in the output. I just need clean essay in the output
+    Donot include any reasoning steps or thoughts, Generate a short 4 lines essay on dogs , do not include chain of thought or think in the output. I just need clean essay in the output
     """
 
 headers = {
@@ -25,6 +25,7 @@ data = {
     }
 
 response = requests.post("https://api.together.xyz/v1/completions", headers=headers, json=data, timeout=240)
+print(type(response))
 if response.status_code == 200:
         kundali_text = response.json()['choices'][0]['text'].strip()
         print(kundali_text)
